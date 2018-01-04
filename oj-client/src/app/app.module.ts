4fb7d83c9ef43bf9e7f58215a0754c0f27c4a59e
xkzero <zxk1995@hotmail.com>
2018-01-03 19:08:51 -0800

    client pages problem list and detail

--- oj-client/src/app/app.module.ts
+++ oj-client/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { routing } from "./app.routes";
 import { AppComponent } from './app.component';
+import { ProblemListComponent } from './components/problem-list/problem-list.component';
 
+import { DataService } from "./services/data.service";
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProblemListComponent,
+    ProblemDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
-  providers: [],
+  providers: [{
+    provide: "data",
+    useClass: DataService
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
